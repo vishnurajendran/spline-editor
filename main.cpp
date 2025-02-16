59660367fa6d12e03d1c200611d6c0bb995eac8e
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2025-02-16 15:24:53 -0500

    gitignore updated and starting point set

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Eigen::MatrixXi F;
 int main(int argc, char *argv[])
 {
     // Load a mesh in OFF format
-    igl::readOFF("/bunny.off", V, F);
+    igl::readOFF("assets/bunny.off", V, F);
 
     // Plot the mesh
     igl::opengl::glfw::Viewer viewer;
